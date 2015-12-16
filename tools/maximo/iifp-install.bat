154de4a903e4acce890d5fe6ea39c91f137e5df6
Martin Nichol <marty@asdepique.ca>
2015-12-16 14:37:18 -0400

    fix tool compile run script in install script

--- tools/maximo/iifp-install.bat
+++ tools/maximo/iifp-install.bat
@@ -9,3 +9,4 @@ REM
 call commonEnv.bat
 
 ..\java\jre\bin\java  -classpath .;.\classes;%MAXIMO_CLASSPATH%;.\lib\jsap.jar;.\lib\jaxen.jar;.\lib\isiframe-tools.jar  com.interlocsolutions.maximo.tools.IFramePortletEnvSetup %*
+internal\runscriptfile.bat -cisiframe -fiframe_001
--- tools/maximo/iifp-install.sh
+++ tools/maximo/iifp-install.sh
@@ -8,3 +8,4 @@ pathToScript=`dirname %0`
 . $pathToScript/commonenv.sh
 
 ../java/jre/bin/java -classpath .:./classes:$MAXIMO_CLASSPATH:./lib/jsap.jar:./lib/jaxen.jar  com.interlocsolutions.maximo.tools.InstallerEnvSetup "$*"
+internal/runscriptfile.sh -cisiframe -fiframe_001
