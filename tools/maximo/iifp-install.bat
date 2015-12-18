a060e9bc05eb61f188ae102b591d4a5bd5fbd0cc
Martin Nichol <marty@asdepique.ca>
2015-12-18 10:26:58 -0400

    fixed install script problems

--- tools/maximo/iifp-install.bat
+++ tools/maximo/iifp-install.bat
@@ -9,4 +9,5 @@ REM
 call commonEnv.bat
 
 ..\java\jre\bin\java  -classpath .;.\classes;%MAXIMO_CLASSPATH%;.\lib\jsap.jar;.\lib\jaxen.jar;.\lib\isiframe-tools.jar;.\lib\MaximoPortlet-tools.jar  com.interlocsolutions.maximo.tools.IFramePortletEnvSetup %*
-internal\runscriptfile.bat -cisiframe -fiframe_001
+cd internal
+.\runscriptfile.bat -cisiframe -fiframe_001
--- tools/maximo/iifp-install.sh
+++ tools/maximo/iifp-install.sh
@@ -7,5 +7,6 @@
 pathToScript=`dirname %0`
 . $pathToScript/commonenv.sh
 
-../java/jre/bin/java -classpath .:./classes:$MAXIMO_CLASSPATH:./lib/jsap.jar:./lib/jaxen.jar:./lib/MaximoPortlet-tools.jar  com.interlocsolutions.maximo.tools.InstallerEnvSetup "$*"
-internal/runscriptfile.sh -cisiframe -fiframe_001
+../java/jre/bin/java -classpath .:./classes:$MAXIMO_CLASSPATH:./lib/jsap.jar:./lib/jaxen.jar:./lib/isiframe-tools.jar:./lib/MaximoPortlet-tools.jar  com.interlocsolutions.maximo.tools.IFramePortletEnvSetup "$*"
+cd internal
+./runscriptfile.sh -cisiframe -fiframe_001
